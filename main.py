556b284240261d9cb10b6da618f96ffbe2a7b674
Alisher <alisherxujanov163@gmail.com>
2020-10-26 16:02:05 +0300

    with break/cont

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ import random
 if __name__ == "__main__":
     secret_number = random.randint(0,100)
     result = game(secret_number)
-    if result is False:# delete
-        print('You lose!')# delete
-    elif result is True:# delete
-        print('You win!')# delete
--- random_game.py
+++ random_game.py
@@ -5,7 +5,7 @@ def game(secret_num):
         # another method: use "break" and "continue" within the "while" loop
         # Homework: get it working with while true, and no return from the function until game is over and you have printed the score
     times = 0
-    while True: # try using while True:
+    while True:
         guess = int(input("Enter a number please: "))
         times += 1
 
@@ -18,12 +18,15 @@ def game(secret_num):
                 print("Too close, a little below than that...")
             else:
                 print("Too high...")
+            continue
         elif guess < secret_num:
             if guess == secret_num - 1:
                 print("Too close, a little higher than that...")
             else:
                 print("Too below...")
+            continue
         else:
             print(f"It took you {times} {att} in total...")
-            return False
+            break
     # ifyou reach here, it means you ran out of turn
+game(56)
